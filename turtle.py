1a3c7494758c8222bd87105c71d200889a8bb053
uwubb <54524082+uwubb@users.noreply.github.com>
2019-08-25 22:55:01 -0700

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,10 @@
+import turtle
+
+my_turtle = turtle.Turtle()   #create a turtle
+my_turtle.color('green')
+my_turtle.forward(50)
+my_turtle.up()
+my_turtle.forward(50)
+my_turtle.right(90)
+my_turtle.down()
+my_turtle.Backward(100)
