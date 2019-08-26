cfc7a87f1080be9c0070eb5ff657f024c0b73b10
uwubb <54524082+uwubb@users.noreply.github.com>
2019-08-25 23:26:43 -0700

    Create epic-turtle.py

---
+++ epic-turtle.py
@@ -0,0 +1,25 @@
+#   Epic Turtle
+#
+#   https://docs.python.org/2/library/turtle.html
+#
+
+import turtle
+
+count = 0
+while count < 5:
+    my_turtle = turtle.Turtle()   # create a turtle
+    my_turtle.color("blue")      # set the color
+    my_turtle.speed(1)
+    my_turtle.forward(50)
+    my_turtle.color("green")      # set the color
+    my_turtle.up()
+    my_turtle.forward(50)
+    my_turtle.color("red")      # set the
+    my_turtle.right(90)
+    my_turtle.forward(50)
+    my_turtle.color("orange")      # set the color
+    my_turtle.right(90)
+    my_turtle.forward(50)
+    my_turtle.color("yellow")      # set the color
+    my_turtle.down()
+    my_turtle.color("pink")      # set the color
