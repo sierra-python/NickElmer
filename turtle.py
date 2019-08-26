4e6f5ed886baa2200f4a719df70faa8ecd014f12
uwubb <54524082+uwubb@users.noreply.github.com>
2019-08-25 22:57:24 -0700

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,10 +1,10 @@
 import turtle
 
-my_turtle = turtle.Turtle()   #create a turtle
-my_turtle.color('green')
-my_turtle.forward(50)
-my_turtle.up()
-my_turtle.forward(50)
-my_turtle.right(90)
-my_turtle.down()
-my_turtle.Backward(100)
+my_turtle = turtle.Turtle()   # create a turtle
+my_turtle.color('green')      # set the color
+my_turtle.forward(50)         # draw a green line of length 50
+my_turtle.up()                # lift up the tail
+my_turtle.forward(50)         # move forward 50 without drawing
+my_turtle.right(90)           # change direction to the right, left works too
+my_turtle.down()              # put the tail down
+my_turtle.Backward(100)       # draw a green line 100 units long
