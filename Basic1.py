aa1c6d61040d7a75efb0fb1069c441aab8b36d37
NickElmer1 <54524082+NickElmer1@users.noreply.github.com>
2019-09-06 00:43:16 -0700

    Create Basic1.py

---
+++ Basic1.py
@@ -0,0 +1,7 @@
+#Store a message in a variable, and print that message. Then change the value of your variable to a new message, and print the new message.
+
+text="m e s s a g e"
+print(text)
+text="M E S S A G E"
+print(text)
+
