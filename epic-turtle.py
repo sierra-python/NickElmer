ff714444f2724cd9bd81702ba99d3a5f4a582fc6
NickElmer1 <54524082+NickElmer1@users.noreply.github.com>
2019-09-05 23:57:57 -0700

    beta-0.2

--- epic-turtle.py
+++ epic-turtle.py
@@ -7,19 +7,75 @@ import turtle
 
 
 while True:
-    my_turtle = turtle.Turtle()   # create a turtle
-    my_turtle.color("blue")      # set the color
+    my_turtle = turtle.Turtle()
+    #To get to the starting point for S
+    my_turtle.color("white")   # create a turtle
+    my_turtle.setpos(-250,100)
+    #The Letter S
+    my_turtle.color("red")      # set the color
     my_turtle.speed(1)
-    my_turtle.forward(50)
-    my_turtle.color("green")      # set the color
-    my_turtle.up()
-    my_turtle.forward(50)
-    my_turtle.color("red")      # set the
-    my_turtle.right(90)
-    my_turtle.forward(50)
-    my_turtle.color("orange")      # set the color
-    my_turtle.right(90)
-    my_turtle.forward(50)
-    my_turtle.color("yellow")      # set the color
-    my_turtle.down()
-    my_turtle.color("pink")      # set the color
+    my_turtle.bk(50)
+    my_turtle.setpos(-300,50)
+    my_turtle.setpos(-250,50)
+    my_turtle.setpos(-250,0)
+    my_turtle.setpos(-300,0)
+    #To get to the starting point for I
+    my_turtle.color("white")   # create a turtle
+    my_turtle.setpos(-225,100)
+    #The Letter I
+    my_turtle.color("blue")
+    my_turtle.setpos(-225,0)
+    #To get to the starting point for E
+    my_turtle.color("white")   # create a turtle
+    my_turtle.setpos(-150,100)
+    #The Letter E
+    my_turtle.color("purple")
+    my_turtle.setpos(-200,100)
+    my_turtle.setpos(-200,0)
+    my_turtle.setpos(-150,0)
+    my_turtle.color("white")
+    my_turtle.setpos(-200,50)
+    my_turtle.color("purple")
+    my_turtle.setpos(-150,50)
+    #To get to the starting point for R
+    my_turtle.color("white")   # create a turtle
+    my_turtle.setpos(-125,100)
+    #The Letter R
+    my_turtle.color("green")
+    my_turtle.setpos(-125,0)
+    my_turtle.color("white")
+    my_turtle.setpos(-126,0)
+    my_turtle.setpos(-126,100)
+    my_turtle.color("green")
+    my_turtle.setpos(-75,100)
+    my_turtle.setpos(-75,50)
+    my_turtle.setpos(-125,50)
+    my_turtle.setpos(-75,0)
+
+    #To get to the starting point for R
+    my_turtle.color("white")   # create a turtle
+    my_turtle.setpos(-50,100)
+    #The Letter R
+    my_turtle.color("orange")
+    my_turtle.setpos(-50,0)
+    my_turtle.color("white")
+    my_turtle.setpos(-51,0)
+    my_turtle.setpos(-51,100)
+    my_turtle.color("orange")
+    my_turtle.setpos(-0,100)
+    my_turtle.setpos(-0,50)
+    my_turtle.setpos(-50,50)
+    my_turtle.setpos(-0,0)
+
+    #To get to the starting point for A
+    my_turtle.color("white")   # create a turtle
+    my_turtle.setpos(25,0)
+    #The Letter A
+    my_turtle.color("pink")
+    my_turtle.setpos(25,100)
+    my_turtle.setpos(75,100)
+    my_turtle.setpos(75,0)
+    my_turtle.color("white")
+    my_turtle.setpos(25,50)
+    my_turtle.color("pink")
+    my_turtle.setpos(600,50)
