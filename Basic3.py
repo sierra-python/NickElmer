f113b1c9f4f968578f2e05228bdc3ff425c32f7d
NickElmer1 <54524082+NickElmer1@users.noreply.github.com>
2019-09-06 00:43:55 -0700

    Create Basic3.py

---
+++ Basic3.py
@@ -0,0 +1,4 @@
+#Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+number="12"
+print("My Favorite number is", number)
