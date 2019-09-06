a984e251e74a2fcf85858e246bafac0af0f13715
NickElmer1 <54524082+NickElmer1@users.noreply.github.com>
2019-09-06 00:43:40 -0700

    Create Basic2.py

---
+++ Basic2.py
@@ -0,0 +1,8 @@
+#Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+#Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
+name="Steve Jobs"
+quote='"Great things in business are never done by one person. Theyre done by a team of people."'
+
+print(name,"once said,",quote)
